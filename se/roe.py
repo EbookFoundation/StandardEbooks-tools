7d8123f7dbf673196777e892d3cb4e0621f4626f
maxmelo <mmelo@stevens.edu>
2019-03-11 19:26:17 -0400

    remove old functions

--- se/roe.py
+++ se/roe.py
@@ -6,12 +6,6 @@ import json
 import se
 from dotenv import load_dotenv
 
-REQUIRED_FIELDS = {
-    "title": "title",
-    "author": "creator",
-    "isbn": "",
-    "version": "meta;se:revision-number"
-}
 POST_URL = "http://ec2-18-219-223-27.us-east-2.compute.amazonaws.com/api/publish"
 
 # Retrieve key and secret from environment variables
@@ -36,39 +30,6 @@ def post_to_roe(url, d, key, secret):
     req.add_header('Content-Length', len(je))
 
     return urllib.request.urlopen(req, je)
-
-# extract relevant roe data from xml metadata tree
-def extract_roe_content_old(tree):
-    data = {}
-    try:
-        #parse content xml and get metadata
-        root = tree.getroot()
-        metadata = [x for x in root if x.tag[-8:] == "metadata"][0]
-        
-        #iterate through metadata and extract relevant fields by tag
-        #if field has semicolon, match tag and also property (there are many meta tags but they have different property attributes)
-        for child in metadata:
-            for key, value in REQUIRED_FIELDS.items():
-                spl = value.split(";")
-                tag = spl[0]
-
-                if tag == child.tag[-len(tag):]:
-                    if len(spl) == 1:
-                        data[key] = child.text
-                        break
-                    else:
-                        if not "property" in child.attrib or child.attrib["property"] != spl[1]:
-                            continue
-
-                        data[key] = child.text
-
-        #remove url prefix from source attribute
-        if "source" in data and data["source"][:4] == "url:":
-            data["source"] = data["source"][4:]
-    except:
-        raise se.InvalidSeEbookException
-
-    return data
 
 def extract_roe_content(metadata_tree):
     d = {}
